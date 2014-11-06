9874ee34860db6d8d37dd05652244bb3dbd77569
Ubuntu <ubuntu@ip-172-31-35-166.us-west-2.compute.internal>
2014-11-06 01:32:41 +0000

    update for vote.py

--- vote.py
+++ vote.py
@@ -2,23 +2,43 @@
 import commands
 counter =0
 while counter <=100 :
-  #alocate new Elastic IP, and get the allocation id
-  (stauts,output) = commands.getstatusoutput("aws ec2 allocate-address")
+  (stauts,output) = commands.getstatusoutput("/usr/local/bin/aws ec2 describe-addresses ")
+  if len(output) > 5 :
+    print output
+    break;
+  #alocate new Elastic IP, and get the allocation id and allocated ip
+  (stauts,output) = commands.getstatusoutput("/usr/local/bin/aws ec2 allocate-address")
   allocation_id = output.split('\t') [0]
+  new_gip =  output.split('\t') [2]
+
+  #Sleep for 2 seconds
+  (status,output) = commands.getstatusoutput("/bin/sleep 2")
 
   #associate the allocated ip to indicated ec2 instance
-  (status,output) = commands.getstatusoutput("aws ec2 associate-address --instance-id i-9afe2b90 --allocation-id "+allocation_id)
+  (status,output) = commands.getstatusoutput("/usr/local/bin/aws ec2 associate-address --instance-id i-9afe2b90 --allocation-id "+allocation_id)
+  print status,output
+  association_id = output;
 
   #Sleep for 3 seconds
-  (status,output) = commands.getstatusoutput("sleep 3")
+  (status,output) = commands.getstatusoutput("/bin/sleep 5")
  
- #click the URL!!
- (status,output) = commands.getstatusoutput("wget http://star.sznsibi.org/vote.ashx?id=2560")
+  #click the URL!!
+  (status,output) = commands.getstatusoutput("/usr/bin/wget http://star.sznsibi.org/vote.ashx?id=2560")
+  print counter, new_gip
   #Sleep for 2 seconds
-  (status,output) = commands.getstatusoutput("sleep 2")
+  (status,output) = commands.getstatusoutput("/bin/sleep 10")
+
+  #Disassociate Elastic IP address
+  (status,output) = commands.getstatusoutput("/usr/local/bin/aws ec2 disassociate-address --association-id " + association_id)
+
+  #Sleep for 3 seconds
+  (status,output) = commands.getstatusoutput("/bin/sleep 3")
 
   #release allocated Elastic IP
-  (status,output) = commands.getstatusoutput("aws ec2 release-address --allocation-id " + allocation_id)
+  (status,output) = commands.getstatusoutput("/usr/local/bin/aws ec2 release-address --allocation-id " + allocation_id)
+  
+  #Sleep for 3 seconds
+  (status,output) = commands.getstatusoutput("/bin/sleep 3")
+
   counter +=1
-  print counter
 
