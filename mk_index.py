2f62bc22ff0126d55722b92ef5b20ea933812d64
Cong Yue <cong_yue@alliedtelesis.com>
2015-02-23 17:17:03 -0800

    Change to use helper function for better bulk result.

--- mk_index.py
+++ mk_index.py
@@ -1,6 +1,7 @@
 # build_index.py
 import datetime
 from elasticsearch import Elasticsearch
+from elasticsearch import helpers
 
 FILE_PATH = "./squid.log.1"
 ES_HOST = {
@@ -10,7 +11,7 @@ ES_HOST = {
 
 INDEX_NAME = 'ats'
 TYPE_NAME = 'accesslog'
-COMMIT_DATA_PER_TIME =5
+COMMIT_DATA_PER_TIME =5000
 bulk_data = []
 
 # create ES client, create index
@@ -87,6 +88,7 @@ def indexBulkData():
         #one example
         # ['1424376277.821', '0', '10.0.0.210', 'TCP_MEM_HIT/200',
         # '86949', 'GET', 'http://www.citrix.co.jp/products.html?posit=glnav', '-', 'NONE/-', 'text/html']
+        """
         op_dict = {
             "index": {
                 "_index": INDEX_NAME, 
@@ -94,42 +96,57 @@ def indexBulkData():
                 "_id": items[0]
             }
         }
-        data_dict = {
-            'accessTime': datetime.datetime.fromtimestamp(float(items[0])).strftime('%Y-%m-%dT%H:%M:%SZ'), #The client request timestamp
-            'spentTime': items[1], #The time Traffic Server spent processing the client request. 
-                                   #The number of milliseconds between the time the client established the connection with Traffic Server 
-                                   #and the time Traffic Server sent the last byte of the response back to the client.
-            'clientIP': items[2], #The IP address of the client's host machine.
-            'cacheResult': items[3].split('/')[0], #The cache result code; how the cache responded to the request: HIT, MISS, and so on. 
-            'responseCode': items[3].split('/')[1], #The proxy response status code (the HTTP response status code from Traffic Server to client
-            'contentLength': items[4], #The length of the Traffic Server response to the client in bytes, including headers and content.
-            'requestMethod': items[5], #The client request method: GET, POST, and so on.
-            'requestURL': items[6], #The client request canonical URL; 
-                                   #blanks and other characters that might not be parsed by log analysis tools are 
-                                   #replaced by escape sequences. The escape sequence is a percentage sign 
-                                   #followed by the ASCII code number of the replaced character in hex.
-            'authUserName': items[7], #The username of the authenticated client. 
-                                      #A hyphen (-) means that no authentication was required.
-            'hierarchyRoute': items[8].split('/')[0], # The proxy hierarchy route. 
-            'routerServer': items[8].split('/')[1], # The route Traffic Server used to retrieve the object. 
-            'contentType': items[9] # The proxy response content type. The object content type taken from the Traffic Server response header.
+
+        op_dict = {
+            "_index": INDEX_NAME, 
+            "_type": TYPE_NAME, 
+            "_id": items[0],
         }
-        bulk_data.append(op_dict)
+        """
+        data_dict = {
+        "_index": INDEX_NAME, 
+        "_type": TYPE_NAME, 
+        "_id": items[0],        
+        "_source": {
+                'accessTime': datetime.datetime.fromtimestamp(float(items[0])).strftime('%Y-%m-%dT%H:%M:%SZ'), #The client request timestamp
+                'spentTime': items[1], #The time Traffic Server spent processing the client request. 
+                                       #The number of milliseconds between the time the client established the connection with Traffic Server 
+                                       #and the time Traffic Server sent the last byte of the response back to the client.
+                'clientIP': items[2], #The IP address of the client's host machine.
+                'cacheResult': items[3].split('/')[0], #The cache result code; how the cache responded to the request: HIT, MISS, and so on. 
+                'responseCode': items[3].split('/')[1], #The proxy response status code (the HTTP response status code from Traffic Server to client
+                'contentLength': items[4], #The length of the Traffic Server response to the client in bytes, including headers and content.
+                'requestMethod': items[5], #The client request method: GET, POST, and so on.
+                'requestURL': items[6], #The client request canonical URL; 
+                                       #blanks and other characters that might not be parsed by log analysis tools are 
+                                       #replaced by escape sequences. The escape sequence is a percentage sign 
+                                       #followed by the ASCII code number of the replaced character in hex.
+                'authUserName': items[7], #The username of the authenticated client. 
+                                          #A hyphen (-) means that no authentication was required.
+                'hierarchyRoute': items[8].split('/')[0], # The proxy hierarchy route. 
+                'routerServer': items[8].split('/')[1], # The route Traffic Server used to retrieve the object. 
+                'contentType': items[9] # The proxy response content type. The object content type taken from the Traffic Server response header.
+            }
+        }
+        #bulk_data.append(op_dict)
         bulk_data.append(data_dict)
         line_cnt = line_cnt +1
         if line_cnt == COMMIT_DATA_PER_TIME:
             # bulk index the data
             #print("bulk indexing...")
             #print(bulk_data)
-            res = es.bulk(index = INDEX_NAME, body = bulk_data, refresh = True)
+            #res = es.bulk(index = INDEX_NAME, body = bulk_data, refresh = True)
+            helpers.bulk(es,bulk_data)
+            es.indices.refresh()
             #print(" response: '%s'" % (res))
             line_cnt =0
             bulk_data = []
             #res = es.count(index= INDEX_NAME, doc_type= TYPE_NAME, body={"query": {"match_all": {}}})
             #print(" response: '%s'" % (res))
 if len(bulk_data) >0:
-    res = es.bulk(index = INDEX_NAME, body = bulk_data, refresh = True)
-
+    #res = es.bulk(index = INDEX_NAME, body = bulk_data, refresh = True)
+    res = helpers.bulk(es,bulk_data)
+    es.indices.refresh()
     # sanity check
     print("searching...")
     res = es.search(index = INDEX_NAME, size=3, body={"query": {"match_all": {}}})
@@ -137,4 +154,5 @@ if len(bulk_data) >0:
 
 indexPrepare()
 indexBulkData()
+
 
