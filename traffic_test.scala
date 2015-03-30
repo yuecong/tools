18a095536d9fe64c9c3807e00b61605575bfd0b9
Cong Yue <cong_yue@alliedtelesis.com>
2015-03-30 16:03:40 -0700

    change it to a obeject

--- traffic_test.scala
+++ traffic_test.scala
@@ -1,35 +1,39 @@
 import sys.process._
 import java.io.IOException
 
-val MAX_THREAD = args(0).toInt;
-var totalTime:Long = 0;
-val now = System.nanoTime;
-//println(MAX_THREAD);
-def readURL()= {
-  val url1 = "https://www.yahoo.com/";
-  //val url1 ="http://10.4.17.63:8081/chart1.html";
-  try{
-   io.Source.fromURL(url1)
-  } catch{
-      case e: Exception => { e.printStackTrace(); e.toString() }
-  } finally {
-  val processTime = (System.nanoTime - now) /1000/1000; //million seconds
-  totalTime = List(totalTime,processTime).max;
-  println(" Total Process Time: %d ms".format(totalTime));
+object trafficTest {
+
+  def main(args:Array[String]) {
+
+    val MAX_THREAD = args(0).toInt;
+    var totalTime:Long = 0;
+    val now = System.nanoTime;
+
+    def readURL()= {
+      //val url1 = "https://www.yahoo.com/";
+      val url1 ="http://10.4.17.63:8081/chart1.html";
+      try{
+       io.Source.fromURL(url1)
+      } catch{
+          case e: Exception => { e.printStackTrace(); e.toString() }
+      } finally {
+         val processTime = (System.nanoTime - now) /1000/1000; //million seconds
+         totalTime = List(totalTime,processTime).max;
+         println(" Total Process Time: %d ms".format(totalTime));
+      }
+    }
+ 
+    // get URL cocurrently 
+    for (i <- 1 to  MAX_THREAD ) {
+        val thread = new Thread {
+            override def run {
+                readURL();
+            }
+        }
+        thread.start
+        //Thread.sleep(50) // slow the loop down a bit
+    }
   }
 }
-// get URL cocurrently 
-for (i <- 1 to  MAX_THREAD ) {
-    val thread = new Thread {
-        override def run {
-            readURL();
-        }
-    }
-    thread.start
-    //Thread.sleep(50) // slow the loop down a bit
-}
-
-//println("HERE!!!!!");
-
  
 
